a4ff006174d3ec07e0d747ccc61f854cdbed0f0a
Chun Gong <cgong@c02ytd84lvcf.lan>
2022-07-03 10:16:44 -0500

    one more solved

--- USACO/202102/comfortablcows.py
+++ USACO/202102/comfortablcows.py
@@ -1,59 +1,71 @@
 n = int(input())
-board = []
-for i in range(n):
-    row = []
-    for j in range(n):
-        row.append(0)
-    board.append(row)
-
+# The idea is to use a dict as the board and the tuple of coordinators (x, y) as the key.
+# The value of the dict is a list of two values, first value could be 0 or 1 to indicate if that position has been taken
+# The second value could be 0, 1, 2, 3, 4, indicating the neighbors.
+# When a position (x, y) has a vlue, [1, 3], it is a comfortable cow. Note that if the value is [1, 4], we need to
+# decrease the number of comfortable cows by one.
+board = {}
 count = 0
 
-
-def check(x, y):
-    m = 0
-    if x > 0 and board[x-1][y]:
-        m += 1
-    if y > 0 and board[x][y-1]:
-        m += 1
-    if x < n - 1 and board[x+1][y]:
-        m += 1
-    if y < n - 1 and board[x][y+1]:
-        m += 1
-    return m
-
-
 for i in range(n):
-    p = [int(d) for d in input().split()]
-    x = p[0]
-    y = p[1]
-    board[x][y] = 1
-
-    if check(x, y) == 3:
-        count += 1
+    q = [int(d) for d in input().split()]
+    x = q[0]
+    y = q[1]
+    p = x, y
+    if p not in board:
+        board[p] = [1, 0]
+    else:
+        board[p][0] += 1
+        if board[p][1] == 3:
+            count += 1
     if x > 0:
-        u = check(x - 1, y)
-        if u == 3:
-            count += 1
-        elif u == 4:
-            count -= 1
-    if y > 0:
-        u = check(x, y - 1)
-        if u == 3:
-            count += 1
-        elif u == 4:
-            count -= 1
+        key = (x - 1, y)
+        if key not in board:
+            board[key] = [0, 1]
+        else:
+            elem = board[key]
+            elem[1] += 1
+            if elem[0] == 1:
+                if elem[1] == 3:
+                    count += 1
+                elif elem[1] == 4:
+                    count -= 1
     if x < n - 1:
-        u = check(x+1, y)
-        if u == 3:
-            count += 1
-        elif u == 4:
-            count -= 1
+        key = (x + 1, y)
+        if key not in board:
+            board[key] = [0, 1]
+        else:
+            elem = board[key]
+            elem[1] += 1
+            if elem[0] == 1:
+                if elem[1] == 3:
+                    count += 1
+                elif elem[1] == 4:
+                    count -= 1
+    if y > 0:
+        key = (x, y - 1)
+        if key not in board:
+            board[key] = [0, 1]
+        else:
+            elem = board[key]
+            elem[1] += 1
+            if elem[0] == 1:
+                if elem[1] == 3:
+                    count += 1
+                elif elem[1] == 4:
+                    count -= 1
     if y < n - 1:
-        u = check(x, y + 1)
-        if u == 3:
-            count += 1
-        elif u == 4:
-            count -= 1
+        key = (x, y + 1)
+        if key not in board:
+            board[key] = [0, 1]
+        else:
+            elem = board[key]
+            elem[1] += 1
+            if elem[0] == 1:
+                if elem[1] == 3:
+                    count += 1
+                elif elem[1] == 4:
+                    count -= 1
 
     print(count)
 
--- USACO/202102/fence.py
+++ USACO/202102/fence.py
@@ -2,26 +2,23 @@ n = int(input())
 for i in range(n):
     path = input()
     l = len(path)
-    points = set()
     current_x = 0
     current_y = 0
-    answer = "CW"
+
     for j in range(l):
         s = path[j]
         if s == "N":
             current_y += 1
+            last_n = current_x
         elif s == "S":
             current_y -= 1
+            last_s = current_x
         elif s == "E":
             current_x += 1
         else:
             current_x -= 1
-        if (current_x, current_y) in points:
-            answer = "CCW"
-            break
-        else:
-            points.add((current_x, current_y))
-    if current_x != 0 or current_y != 0:
-        answer = "CCW"
+    if last_n > last_s:
+        print("CCW")
+    else:
+        print("CW")
 
-    print(answer)
