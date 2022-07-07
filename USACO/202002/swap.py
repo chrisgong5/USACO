d85d4718a7601e204ba44ff9c92e1356b6cd2be0
Chun Gong <cgong@c02ytd84lvcf.lan>
2022-07-07 10:07:40 -0500

    checkpoint

--- USACO/202002/swap.py
+++ USACO/202002/swap.py
@@ -1,4 +1,6 @@
 import sys
+import copy
+
 sys.stdin = open("swap.in", "r")
 out_f = open("swap.out", "w")
 
@@ -10,6 +12,8 @@ for i in range(n+1):
     cows[i] = i
 
 
+# Give a list, l_cows, this function perform a reverse on
+# the sub-list from index start to index end
 def swap_cows(l_cows, start, end):
     while start < end:
         t = l_cows[start]
@@ -19,9 +23,32 @@ def swap_cows(l_cows, start, end):
         end -= 1
 
 
-if k % 2 == 1:
+# Key observation: if the two intervals r1 and r2 do not interact with each other,
+# then ever two reverse will make the list back to its original one.
+# The tricky part is that, when r1 and r2 has overlap, then when we perform reverse for r1 and r2
+# certain times, it will also become the original one. So we can first try to find home many reverse of r1 and r2
+# turns the list back to its original (assume it's x), then we can do y = k % x, and perform reverse on r1, r2 for
+# y times.
+
+# Make a copy of the original list
+original = cows.copy()
+
+if r1[1] < r2[0] or r1[0] > r2[1]:
+    if k % 2 == 1:
+        swap_cows(cows, r1[0], r1[1])
+        swap_cows(cows, r2[0], r2[1])
+else:
     swap_cows(cows, r1[0], r1[1])
     swap_cows(cows, r2[0], r2[1])
+    x = 1
+    while cows != original:
+        swap_cows(cows, r1[0], r1[1])
+        swap_cows(cows, r2[0], r2[1])
+        x += 1
+    y = k % x
+    for j in range(y):
+        swap_cows(cows, r1[0], r1[1])
+        swap_cows(cows, r2[0], r2[1])
 
 for i in range(1, n+1):
     out_f.write(str(cows[i])+"\n")
